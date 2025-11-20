dad59e230de6b971f3b843f9f894ee55e9b7140f
Bhargavi <bhargavin189@gmail.com>
2025-11-20 02:33:32 -0500

    Fix port mismatch in Hospital B manage-agent
    - Change Dockerfile EXPOSE from 9001 to 8001
    - Update entrypoint.sh default port from 9001 to 8001
    - Matches Cloud Run deployment port configuration

--- Version-2-HospitalB/services/manage_agent/entrypoint.sh
+++ Version-2-HospitalB/services/manage_agent/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-# Use PORT environment variable if set, otherwise default to 9001
-PORT=${PORT:-9001}
+# Use PORT environment variable if set, otherwise default to 8001
+PORT=${PORT:-8001}
 
 exec uvicorn services.manage_agent.main:app --host 0.0.0.0 --port "$PORT"
 
